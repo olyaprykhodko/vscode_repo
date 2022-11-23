0734f7ab71420a6b8fb849dcfd2c99f08740d05e
olyaprykhodko <o.u.prihodko@gmail.com>
2022-11-23 17:13:53 +0200

    ти можеш/не можеш купити алкоголь

--- hello world.js
+++ hello world.js
@@ -1,1 +1,6 @@
-console.log ('Hello, world!')
+let age = 17;
+console.log ('Ідемо до магазину')
+if (age >= 18) {
+    console.log (Ти можеш купити алкоголь);
+}
+console.log ('Повертаємось додому');
